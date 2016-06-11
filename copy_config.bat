014cf0493e4fcd841542b37a67f743d9261a7e03
Arturs Kalnins <Arturs Kalnins>
2016-06-11 22:13:04 +0100

    test commit

--- copy_config.bat
+++ copy_config.bat
@@ -1,6 +1,6 @@
 @echo off
 echo "------------------------------------------------------------"
-xcopy config.xml Bat_run_debug\x86
-xcopy config.xml Bat_run_debug\x64
+xcopy config.xml Bat_run_debug\86x
+xcopy config.xml Bat_run_debug\64x
 echo "------------------------------------------------------------"
 pause
