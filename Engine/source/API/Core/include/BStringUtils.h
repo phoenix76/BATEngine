28c7883446e4b47cc1ba812bad24082156f9503b
Dmitry Yakovlev <djokerxx2013@mail.ru>
2016-06-13 13:30:42 +0400

    add definitions to methods of CBString

--- Engine/source/API/Core/include/BStringUtils.h
+++ Engine/source/API/Core/include/BStringUtils.h
@@ -12,6 +12,7 @@ Returns the string's digital representations.
 
 #include <string>
 #include <sstream>
+#include <codecvt>
 
 #include "BVariables.h"
 
@@ -21,26 +22,30 @@ namespace BEngine
 	{
 	public:
 		//Constructors
-		CBString() : std::string("") {}
+		CBString()							: std::string("") {}
 		//Copy ctor not required, generates automatically
-		CBString(const std::string& in)	: std::string(in) {}
-		CBString(const char* in)		: std::string(in) {}
+		CBString(const std::string& in)		: std::string(in) {}
+		CBString(const std::wstring& in);
+		CBString(const char* in)			: std::string(in) {}
 		CBString(const wchar_t* in);
-		CBString(int8 in)				: std::string(std::to_string(in)) {}
-		CBString(uint8 in)				: std::string(std::to_string(in)) {}
-		CBString(int16 in)				: std::string(std::to_string(in)) {}
-		CBString(uint16 in)				: std::string(std::to_string(in)) {}
-		CBString(int32 in)				: std::string(std::to_string(in)) {}
-		CBString(uint32 in)				: std::string(std::to_string(in)) {}
-		CBString(int64 in)				: std::string(std::to_string(in)) {}
-		CBString(uint64 in)				: std::string(std::to_string(in)) {}
+		CBString(int8 in)					: std::string(std::to_string(in)) {}
+		CBString(uint8 in)					: std::string(std::to_string(in)) {}
+		CBString(int16 in)					: std::string(std::to_string(in)) {}
+		CBString(uint16 in)					: std::string(std::to_string(in)) {}
+		CBString(int32 in)					: std::string(std::to_string(in)) {}
+		CBString(uint32 in)					: std::string(std::to_string(in)) {}
+		CBString(int64 in)					: std::string(std::to_string(in)) {}
+		CBString(uint64 in)					: std::string(std::to_string(in)) {}
 		CBString(float in);
 		CBString(double in);
 		CBString(long double in);
 
+		~CBString() {}
+
 		//Assignment operators
-		//Assignment operator for CBString not required, generates automatically
+		//Assignment operator for CBString(const CBString&) not required, generates automatically
 		CBString& operator=(const std::string& in)	{ (std::string&)*this = in; }
+		CBString& operator=(const std::wstring& in);
 		CBString& operator=(const char* in)			{ (std::string&)*this = in; }
 		CBString& operator=(const wchar_t* in);
 		CBString& operator=(int8 in)				{ (std::string&)*this = std::to_string(in); }
@@ -57,6 +62,7 @@ namespace BEngine
 
 		//Add operators
 		CBString& operator+(const std::string& in)	{ (std::string&)*this += in; }
+		CBString& operator+(const std::wstring& in);
 		CBString& operator+(const char* in)			{ (std::string&)*this += in; }
 		CBString& operator+(const wchar_t* in);
 		CBString& operator+(int8 in)				{ (std::string&)*this += (std::string)CBString(in); }
@@ -71,29 +77,34 @@ namespace BEngine
 		CBString& operator+(double in);
 		CBString& operator+(long double in);
 
+		//Equal operators
+		bool operator==(const std::string& in)		{ return ((std::string&)*this == in); }
+		bool operator==(const char* in)				{ return ((std::string&)*this == std::string(in)); }
+		bool operator==(const std::wstring& in)		{ return (this->asWStr() == in); }
+		bool operator==(const wchar_t* in)			{ return (this->asWStr() == std::wstring(in)); }
+
 		//Getters
-		std::string asStr()			const;
-		const char* asCStr()		const;
+		std::string asStr()			const { return (std::string)*this; }
+		const char* asCStr()		const { return ((std::string)*this).c_str(); }
 		std::wstring asWStr()		const;
 		const wchar_t* asWCStr()	const;
-		int8 asInt8()				const;
-		uint8 asUInt8()				const;
-		int16 asInt16()				const;
-		uint16 asUInt16()			const;
-		int32 asInt32()				const;
-		uint32 asUInt32()			const;
-		int64 asInt64()				const;
-		uint64 asUInt64()			const;
-		float asFloat()				const;
-		double asDbl()				const;
-		long double asLDbl()		const;
+		int8 asInt8()				const { return (int8)std::stoi((std::string&)*this); }
+		uint8 asUInt8()				const { return (uint8)std::stoi((std::string&)*this); }
+		int16 asInt16()				const { return (int16)std::stoi((std::string&)*this); }
+		uint16 asUInt16()			const { return (uint16)std::stoi((std::string&)*this); }
+		int32 asInt32()				const { return (int32)std::stoi((std::string&)*this); }
+		uint32 asUInt32()			const { return (uint32)std::stoi((std::string&)*this); }
+		int64 asInt64()				const { return (int64)std::stoll((std::string&)*this); }
+		uint64 asUInt64()			const { return (uint64)std::stoull((std::string&)*this); }
+		float asFloat()				const { return std::stof((std::string&)*this); }
+		double asDbl()				const { return std::stod((std::string&)*this); }
+		long double asLDbl()		const { return std::stold((std::string&)*this); }
 
 		//Using the base class's methods
+		using std::string::size;
 		using std::string::length;
 		using std::string::clear;
 		using std::string::erase;
-
-		~CBString() {}
 
 	private:
 		std::stringstream m_dataConverter;
--- Engine/source/API/Core/source/BStringUtils.cpp
+++ Engine/source/API/Core/source/BStringUtils.cpp
@@ -2,5 +2,62 @@
 
 namespace BEngine
 {
-	
+	CBString::CBString(const wchar_t* in)
+	{
+		std::wstring temp(in);
+		(std::string&)*this = std::string(temp.begin(), temp.end());
+	}
+	CBString::CBString(float in)
+	{
+		m_dataConverter << in;
+		m_dataConverter >> (std::string&)*this;
+	}
+	CBString::CBString(double in)
+	{
+		m_dataConverter << in;
+		m_dataConverter >> (std::string&)*this;
+	}
+	CBString::CBString(long double in)
+	{
+		m_dataConverter << in;
+		m_dataConverter >> (std::string&)*this;
+	}
+	CBString& CBString::operator=(const wchar_t* in)
+	{
+		std::wstring temp(in);
+		(std::string&)*this = std::string(temp.begin(), temp.end());
+	}
+	CBString& CBString::operator+(const wchar_t* in)
+	{
+		std::wstring temp(in);
+		(std::string&)*this += std::string(temp.begin(), temp.end());
+	}
+	CBString& CBString::operator+(float in)
+	{
+		
+		m_dataConverter << in;
+		(std::string&)*this += m_dataConverter.str();
+	}
+	CBString& CBString::operator+(double in)
+	{
+
+		m_dataConverter << in;
+		(std::string&)*this += m_dataConverter.str();
+	}
+	CBString& CBString::operator+(long double in)
+	{
+
+		m_dataConverter << in;
+		(std::string&)*this += m_dataConverter.str();
+	}
+	std::wstring CBString::asWStr() const
+	{
+		typedef std::codecvt<wchar_t, char, mbstate_t> codec_t;
+		return std::wstring_convert<codec_t>(&std::use_facet<codec_t>(std::locale(".1251"))).from_bytes(((std::string*)this)->c_str());
+	}
+	const wchar_t* CBString::asWCStr() const
+	{
+		typedef std::codecvt<wchar_t, char, mbstate_t> codec_t;
+		return (std::wstring_convert<codec_t>(&std::use_facet<codec_t>(std::locale(".1251"))).from_bytes(((std::string*)this)->c_str())).c_str();
+	}
 } //namespace BEngine
