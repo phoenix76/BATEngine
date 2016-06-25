9e76e8adb4e7727e1ccf0266d681be0c8b2eafce
Arturs Kalnins <Arturs Kalnins>
2016-06-26 00:01:15 +0100

    Test Add

---
+++ Engine/Engine/ExperimentalNew2.h
@@ -0,0 +1,25 @@
+/***********************************************************************
+TESTNEW2
+
+
+***********************************************************************/
+
+#ifndef TESTNEW2_H
+#define TESTNEW2_H
+
+namespace BEngine 
+{
+	class TestNew2 
+	{
+		public:
+			TestNew2() {}
+			~TestNew2() {}
+			
+		protected:
+		
+		private:
+		
+	};
+} //namespace BEngine
+#endif //TESTNEW2_H
+
