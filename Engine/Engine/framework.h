a9ed3ddc193f9a382b4cd6407f1bd2e73c119176
Dmitry Yakovlev <djokerxx2013@mail.ru>
2016-06-26 02:39:07 +0400

    test

---
+++ Engine/Engine/framework.h
@@ -0,0 +1,40 @@
+/***********************************************************************
+DFE
+
+
+***********************************************************************/
+
+#ifndef DFE_H
+#define DFE_H
+
+namespace BEngine 
+{
+	class dfe 
+	{
+		public:
+			dfe();
+			~dfe();
+			
+		protected:
+		
+		private:
+		
+	};
+} //namespace BEngine
+#endif //DFE_H
+
+#include "stdafx.h"
+#include "dfe.h"
+
+namespace BEngine 
+{
+	dfe::dfe() 
+	{
+	
+	}
+	
+	dfe::~dfe() 
+	{
+	
+	}
+} //namespace BEngine
