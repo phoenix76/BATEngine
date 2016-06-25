9c4aec5809d56d067e76d86bc063ab9c77cc2ce9
Arturs Kalnins <Arturs Kalnins>
2016-06-26 00:04:01 +0100

    Test Add

---
+++ Engine/Engine/ExperimentalAnotherX.h
@@ -0,0 +1,25 @@
+/***********************************************************************
+EXPANOTHER
+
+
+***********************************************************************/
+
+#ifndef EXPANOTHER_H
+#define EXPANOTHER_H
+
+namespace BEngine 
+{
+	class ExpAnother
+	{
+	public:
+		ExpAnother() {}
+		~ExpAnother() {}
+			
+		protected:
+		
+		private:
+		
+	};
+} //namespace BEngine
+#endif //EXPANOTHER_H
+
