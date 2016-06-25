659399384327abbd36a34efe599afd692b2671fc
Arturs Kalnins <Arturs Kalnins>
2016-06-25 23:16:21 +0100

    Test

---
+++ Engine/Engine/Experimental1.h
@@ -0,0 +1,24 @@
+/***********************************************************************
+EXPERIMENTALCLASS
+
+
+***********************************************************************/
+
+#ifndef EXPERIMENTALCLASS_H
+#define EXPERIMENTALCLASS_H
+
+namespace BEngine 
+{
+	class ExperimentalClass 
+	{
+		public:
+			ExperimentalClass() {};
+			~ExperimentalClass() {};
+			
+		protected:
+		
+		private:
+		
+	};
+} //namespace BEngine
+#endif //EXPERIMENTALCLASS_H
