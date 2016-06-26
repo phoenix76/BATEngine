7f675302db9a4f14b1e503fe6f4ae2777fd363d3
Arturs Kalnins <Arturs Kalnins>
2016-06-26 22:45:40 +0100

    Experimental

---
+++ Engine/Engine/ExpNewTestX.h
@@ -0,0 +1,24 @@
+/***********************************************************************
+EXPTESTXNEW1
+
+
+***********************************************************************/
+
+#ifndef EXPTESTXNEW1_H
+#define EXPTESTXNEW1_H
+
+namespace BEngine 
+{
+	class ExpTestXNew1 
+	{
+		public:
+			ExpTestXNew1() {}
+			~ExpTestXNew1() {}
+			
+		protected:
+		
+		private:
+		
+	};
+} //namespace BEngine
+#endif //EXPTESTXNEW1_H
