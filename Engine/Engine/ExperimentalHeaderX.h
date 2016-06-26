1dd733dd48067438f8556efcd8ae15965de5a6b7
Arturs Kalnins <Arturs Kalnins>
2016-06-26 22:07:31 +0100

    Experimental

---
+++ Engine/Engine/ExperimentalHeaderX.h
@@ -0,0 +1,24 @@
+/***********************************************************************
+EXPCLHX
+
+
+***********************************************************************/
+
+#ifndef EXPCLHX_H
+#define EXPCLHX_H
+
+namespace BEngine 
+{
+	class ExpCLHX
+	{
+	public:
+		ExpCLHX() {}
+		~ExpCLHX() {}
+			
+		protected:
+		
+		private:
+		
+	};
+} //namespace BEngine
+#endif //EXPCLHX_H
