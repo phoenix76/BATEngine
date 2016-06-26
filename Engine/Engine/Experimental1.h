6bc3edaf6d79363d9502196c26b5e850ddc4ca79
Arturs Kalnins <Arturs Kalnins>
2016-06-26 21:44:58 +0100

    Experimental

---
+++ Engine/Engine/Experimental1.h
@@ -0,0 +1,24 @@
+/***********************************************************************
+EXPERIMENTAL1
+
+
+***********************************************************************/
+
+#ifndef EXPERIMENTAL1_H
+#define EXPERIMENTAL1_H
+
+namespace BEngine 
+{
+	class Experimental1 
+	{
+		public:
+			Experimental1() {}
+			~Experimental1() {}
+			
+		protected:
+		
+		private:
+		
+	};
+} //namespace BEngine
+#endif //EXPERIMENTAL1_H
