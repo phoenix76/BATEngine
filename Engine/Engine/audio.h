afaf5f8c2b9e626483c7ca906bdc5a4da353072b
Dmitry Yakovlev <djokerxx2013@mail.ru>
2016-06-27 01:35:50 +0400

    test

---
+++ Engine/Engine/audio.h
@@ -0,0 +1,40 @@
+/***********************************************************************
+AUDIO
+
+
+***********************************************************************/
+
+#ifndef AUDIO_H
+#define AUDIO_H
+
+namespace BEngine 
+{
+	class audio 
+	{
+		public:
+			audio();
+			~audio();
+			
+		protected:
+		
+		private:
+		
+	};
+} //namespace BEngine
+#endif //AUDIO_H
+
+#include "stdafx.h"
+#include "audio.h"
+
+namespace BEngine 
+{
+	audio::audio() 
+	{
+	
+	}
+	
+	audio::~audio() 
+	{
+	
+	}
+} //namespace BEngine
