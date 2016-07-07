173ad876e2d88e63486dd3ba85d0be784ef73626
Arturs Kalnins <Arturs Kalnins>
2016-07-07 19:34:53 +0100

    Added Physics Defintions

---
+++ Engine/source/API/Physics/PhysicsDefinitions.h
@@ -0,0 +1,14 @@
+/***********************************************************************
+PHYSICSDEFINITIONS
+
+***********************************************************************/
+
+#ifndef PHYSICSDEFINITIONS_H
+#define PHYSICSDEFINITIONS_H
+
+namespace BEngine 
+{
+	
+} //namespace BEngine
+#endif //PHYSICSDEFINITIONS_H
+
