76d99b5cdf9799fbc3d1d085aee402f7633c725a
Dmitry Yakovlev <djokerxx2013@mail.ru>
2016-07-07 22:15:13 +0400

    test

--- DevMerger/BeforePush.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo "------------------------------------------------------------"
-xcopy "..\Engine\Engine\Engine.vcxproj" "developers\dev1\Engine.vcxproj.xml"
-xcopy "..\Engine\Engine\Engine.vcxproj.filters" "developers\dev1\Engine.vcxproj.filters.xml"
-pause
--- DevMerger/Merge.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo "------------------------------------------------------------"
-DeveloperMerger.exe
-echo "------------------------------------------------------------"
-xcopy "developers\temp\Engine.vcxproj" "..\Engine\Engine\Engine.vcxproj"
-xcopy "developers\temp\Engine.vcxproj.filters" "..\Engine\Engine\Engine.vcxproj.filters"
-pause
